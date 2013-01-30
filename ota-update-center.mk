5a60c82fc62d83e774351ca2c84ae30f08dabea8
Udaykanth Rapeti <udaykanth.rapeti@gmail.com>
2013-01-30 22:15:50 +0530

    changed ota update center ID.

--- ota-update-center.mk
+++ ota-update-center.mk
@@ -1,9 +1,7 @@
 ADDITIONAL_BUILD_PROPERTIES += \
-      otaupdater.otaid=ivendorcm10 \
+      otaupdater.otaid=udaykanthr \
       otaupdater.otaver=$(CM_NUMERICAL_VERSION) \
-      otaupdater.otatime=$(shell date +%Y%m%d-%H%M)	
+      otaupdater.otatime=$(shell date +%Y%m%d-%H%M)
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/OTAUpdater.apk:system/app/OTAUpdater.apk
-
-
